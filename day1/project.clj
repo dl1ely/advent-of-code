31db9ac380e96a56d0bba7aa1dacfa2150f3f6dd
Stefan Pfeiffer, DL1ELY <stefan@fam-pfeiffer.de>
2015-12-16 17:07:40 +0100

    add eastwood plugin

--- day1/project.clj
+++ day1/project.clj
@@ -3,6 +3,8 @@
   :url "http://adventofcode.com/day/1"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :plugins [[jonase/eastwood "0.2.1"]]
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :main ^:skip-aot day1.core
   :target-path "target/%s"
